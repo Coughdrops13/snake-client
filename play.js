f001cdd81694c9e09bfa308b1c239cb973f80a6d
Connor Roberts <connoobets@gmail.com>
2021-11-03 14:14:16 -0600

    modulated our client side function and added name to our snake

--- play.js
+++ play.js
@@ -1,27 +1,6 @@
 const net = require('net');
+const { connect } = require('./client');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-  });
-
-  conn.setEncoding('utf8');
-  conn.on('connect', data => {
-    console.log('connect data:',data);
-  });
-
-  conn.on('data', data => {
-    console.log(data);
-    conn.end();
-  });
-
-  conn.on('end', data => {
-    console.log('end data:',data);
-  });
-
-  return conn;
-};
 
 console.log('Connecting ...');
 connect();
